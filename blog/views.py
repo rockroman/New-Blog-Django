727cbb373c95aad788a14a59dd3987000cb4f584
rockroman <rock.rakic@gmail.com>
2022-11-29 21:59:08 +0000

    initial deployment

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+    { 
+        'author': 'roman RR',
+        'title': 'blog post 1',
+        'content': 'first post created',
+        'date_posted': '29 th november 2022'
+    },
+
+    { 
+        'author': 'mark RR',
+        'title': 'blog post 2',
+        'content': 'second post created',
+        'date_posted':'9 th november 2022'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html')

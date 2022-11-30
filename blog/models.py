cba8de41ab29e3e2e5406751ba237fc2923284ce
rockroman <rock.rakic@gmail.com>
2022-11-30 18:23:10 +0000

    Add post model and makemigration

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 from django.utils import timezone
 from django.contrib.auth.models import User
+
 
 # Create your models here.
 
@@ -10,5 +12,7 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
     featured_image = CloudinaryField('image', default='placeholder')
+   
     

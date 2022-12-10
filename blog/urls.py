ec08c3a7726edb2c88b29927e0459bc198c4ca25
rockroman <rock.rakic@gmail.com>
2022-12-10 08:24:36 +0000

    Add form for creating post and view for it

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from . import views
+from .views import (
+    home,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 
 
 urlpatterns = [
     path('', views.home.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth.mixins import LoginRequiredMixin 
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin 
 from .models import Post
 from django.views import generic
 from django.views.generic import (
@@ -31,7 +31,8 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-class PostCreateView(LoginRequiredMixin, CreateView):
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
 
@@ -39,6 +40,23 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About us'})
+    return render(request, 'blog/about.html', {'title': 'About'})

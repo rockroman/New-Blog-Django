4c86d362bf2ec2dde9a3e8b54dbc0eb3aebc19bf
rockroman <rock.rakic@gmail.com>
2022-12-13 17:16:51 +0000

    Add a pagination functionality to home page and user posts page

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,8 @@ from .views import (
     PostDetailView,
     PostCreateView,
     PostUpdateView,
-    PostDeleteView
+    PostDeleteView,
+    UserPostListView,
 )
 
 
@@ -16,4 +17,5 @@ urlpatterns = [
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin 
 from .models import Post
 from django.views import generic
@@ -23,6 +24,19 @@ class home(generic.ListView):
 class PostDetailView(DetailView):
     model = Post
     fields = ['title', 'content', 'featured_image']
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'   #<app>/<model>_<view_type>.html
+    
+    paginate_by = 3
+    fields = ['title', 'content', 'featured_image']
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+        
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
